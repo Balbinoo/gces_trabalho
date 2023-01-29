e85ea451e8089826784f4907a7f5089664d9ef49
Rodrigo <rodrigo080599@hotmail.com>
2023-01-29 10:08:43 -0300

    atualizando env

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/rodrigo/Desktop/Trabalho-Individual-2022-2-/env"
+set -gx VIRTUAL_ENV "/home/rodrigo/Desktop/gces_trabalho/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
